b574f8d307d87db733ba064e38e56490326ee928
Louis fields <sfields_10@comcast.net>
2025-03-29 18:58:14 -0700

    Final Week *

--- week 8.js
+++ week 8.js
@@ -11,70 +11,87 @@
 //make
 //model
 
-class Car{
-    constructor(make, model){
-        this.make = make;
-        this.model = model;
-    }
-}  
-//class Menu
-class Menu{
-    constructor(){
-//cars array
-    this.cars = [];
-    }
+class Car {
+  constructor(make, model) {
+      this.make = make;
+      this.model = model;
+  }
 }
 
-  //see a menu
-
-
-  //add cars
-  //prompt for a make
-  //prompt for a model
-  //create new car and push car into array
-
-  addCar();{
-    let carMake = prompt("Enter car make:");
-    let carModel = prompt ("Enter the car model:");
-    this.cars.push(new Car (carMake, carModel));
+class Menu {
+  constructor() {
+      this.cars = [];
   }
 
-  //delete cars
-  deleteCar(){
-    let carIndex = prompt("Enter car index to DELETE")
-    this.cars.splice(carIndex,1);
+  // Add car method
+  addCar() {
+      let carMake = prompt("Enter car make:");
+      let carModel = prompt("Enter the car model:");
+      this.cars.push(new Car(carMake, carModel));
+      alert("Car added successfully!");
+  }
+
+  // Delete car method
+  deleteCar() {
+      let carIndex = prompt("Enter car index to DELETE:");
+      if (carIndex >= 0 && carIndex < this.cars.length) {
+          this.cars.splice(carIndex, 1);
+          alert("Car deleted successfully!");
+      } else {
+          alert("Invalid index. Please try again.");
+      }
+  }
+
+  // View all cars method
+  viewAllCars() {
+      let displayCars = "";
+      for (let i = 0; i < this.cars.length; i++) {
+          displayCars += `${i}) ${this.cars[i].make} ${this.cars[i].model}\n`;
+      }
+      alert(`
+      Car Inventory:
+      ---------------
+      ${displayCars || "No cars available."}
+      `);
+  }
+
+  // Show main menu
+  showMainMenu() {
+      return prompt(`
+      Main Menu:
+      ---------------
+      0) Exit Menu
+      1) Add Car
+      2) Delete Car
+      3) View All Cars
+      `);
+  }
+
+  // Start the menu
+  start() {
+      let selection = this.showMainMenu();
+
+      while (selection != "0") {
+          switch (selection) {
+              case "1":
+                  this.addCar();
+                  break;
+              case "2":
+                  this.deleteCar();
+                  break;
+              case "3":
+                  this.viewAllCars();
+                  break;
+              default:
+                  alert("Invalid option. Please try again.");
+          }
+          selection = this.showMainMenu();
+      }
+      alert("Exiting Menu---goodbye!");
+  }
 }
 
-  //view cars
-
-viewCars();{
-    let displayCars = '';
-    for(let i=0, i < this.cars.length; i++){
-
-       displayCars += '
-       ${this.cars[i].make} ${this.cars[i].model}'
-    
-    }
-    
-    alert('
-    Car Inventory:
-    ----------------
-    ${displayCars} 
-    ');
-}
-
-  //start the menu
-
-//instantiate the menu and invoke the start method
-
-let car1 = new Car("Honda", "Civic");
-console.log (car1);
-
+// Instantiate the Menu and start the program
 let menu = new Menu();
-
-menu.addCar();
-menu.viewCars();
-menu.deleteCar();
-menu.viewCars();
-
+menu.start();
 
